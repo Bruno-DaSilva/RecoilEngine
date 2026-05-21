f63ad80e7490e6e6e8c8a1c9532b92ecea170863
bruno-dasilva <8520801+bruno-dasilva@users.noreply.github.com>
2026-05-21 10:54:20 +0000

    Automatic docker build image update from b0edc2e47e58459a59a5252804604dca615b4d14

--- docker-build-v2/images_versions.sh
+++ docker-build-v2/images_versions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 declare -A image_version
-image_version[amd64-linux]=sha256:981590550802c024306fb8a93ff536bdeb961ef5d68207178471627d6bfaaa63
-image_version[amd64-windows]=sha256:408bdfa82ed3e31fdb96d8f6d386dc3665486607d1b3c993e0b91106a04be87e
+image_version[amd64-linux]=sha256:7427706ea1b147f24798eab493e8f9fbdd13987a69b80787893326012e8f2116
+image_version[amd64-windows]=sha256:b0aab49c9e1d0557c6d8293667c5bc2773f7a0b4170f11af0428185f85d9e176
